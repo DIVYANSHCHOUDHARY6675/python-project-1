5e36fdfebc734d7cc839d88c448bd0e7216dfb1d
DIVYANSHCHOUDHARY6675 <76527055+DIVYANSHCHOUDHARY6675@users.noreply.github.com>
2021-02-25 15:16:24 +0530

    Update Game.py

--- PYTHON PROJECT/Game.py
+++ PYTHON PROJECT/Game.py
@@ -1,5 +1,13 @@
-number=int(input("guess a number between 1 to 9 --"))
-if(number>6):
-    print("congratulation you guessed it correctly!!!")
-else:
-    print("guess a number higher than 6")   
+import random 
+number=random.randint(1,9)
+i=1
+while(i<3):
+  choice=int(input("guess a number between 1 to 9 --"))
+  if (choice==number):
+      print("congratulation you guessed it correctly!!!")
+      break
+  else:
+    print("try again")
+    
+  i=i+1
+  
